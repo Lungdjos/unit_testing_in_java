40c430cda80d2c55169bc1b5e91b912bed7a79a0
Lungdjos <lungudjos@gmail.com>
2023-12-04 18:02:12 +0200

    creating test case for the Person Service

--- src/test/java/com/unittestingproject/unittesting/services/PersonServiceTest.java
+++ src/test/java/com/unittestingproject/unittesting/services/PersonServiceTest.java
@@ -1,4 +1,30 @@
 package com.unittestingproject.unittesting.services;
 
+import com.unittestingproject.unittesting.entities.Person;
+import com.unittestingproject.unittesting.repo.PersonRepository;
+import com.unittestingproject.unittesting.services.implementation.PersonServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
+    @Mock
+    private PersonRepository personRepository;
+    private PersonService personService;
+    @BeforeEach
+    void setUp(){
+        this.personService = new PersonServiceImpl(personRepository);
+    }
+    @Test
+    void getAllPersons(){
+        personService.getAllPerson();
+
+        // asserting the results
+        verify(personRepository).findAll();
+    }
 }
