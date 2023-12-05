c8c8d9fff1525c10bb0a56623e98e763e391b245
Lungdjos <lungudjos@gmail.com>
2023-12-05 18:12:12 +0200

    implementing a mockery test for the repo

--- src/test/java/com/unittestingproject/unittesting/repo/PersonRepositoryTest.java
+++ src/test/java/com/unittestingproject/unittesting/repo/PersonRepositoryTest.java
@@ -1,4 +1,28 @@
 package com.unittestingproject.unittesting.repo;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.booleanThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class PersonRepositoryTest {
+
+    private PersonRepository personRepository;
+    @Before("")
+    public void setUp(){
+        // implementation
+        personRepository = (PersonRepository) MockitoAnnotations.openMocks(this);
+    }
+    @Test
+    void verifyMethod(){
+        var id = 1;
+        verify(personRepository).isPersonExistById(id);
+    }
 }
