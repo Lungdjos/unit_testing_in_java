0a9748cc943308983dd6c9c014fd161cc532cf84
Lungdjos <lungudjos@gmail.com>
2023-12-06 08:25:45 +0200

    adding templates and static files

--- src/main/java/com/unittestingproject/unittesting/controllers/PersonController.java
+++ src/main/java/com/unittestingproject/unittesting/controllers/PersonController.java
@@ -4,6 +4,7 @@ import com.unittestingproject.unittesting.pojos.PersonDto;
 import com.unittestingproject.unittesting.services.PersonService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,12 @@ import java.util.Map;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping(value = "/personController")
+@RequestMapping(value = "/person-controller")
 public class PersonController {
     private final PersonService personService;
+
+    @GetMapping(value = "/")
+    public String signupPage(){ return "login/signup";}
     @PostMapping(value = "/addPerson")
     public ModelAndView creatPerson(@RequestBody PersonDto requestData){
         ModelAndView modelAndView = new ModelAndView("index");
